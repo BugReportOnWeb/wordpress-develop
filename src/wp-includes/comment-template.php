c576999dbd9fdb0f6891bd6a3fe6e1f897978e37
Devasheesh Kaul <devasheesh0909@gmail.com>
2025-08-12 15:20:11 +0530

    fix: escaping of numberic comparison in comment text

--- src/wp-includes/comment-template.php
+++ src/wp-includes/comment-template.php
@@ -1076,6 +1076,16 @@ function comment_text( $comment_id = 0, $args = array() ) {
 
 	$comment_text = get_comment_text( $comment, $args );
 
+	// Encode standalone < and > between digits,
+	// to prevent them being parsed as HTML tags.
+	$comment_text = preg_replace_callback(
+		'/(\d)(\s*)([<>])(\s*)(\d)/',
+		function ( $matches ) {
+			return $matches[1] . $matches[2] . htmlspecialchars( $matches[3] ) . $matches[4] . $matches[5];
+		},
+		$comment_text
+	);
+
 	/**
 	 * Filters the text of a comment to be displayed.
 	 *
